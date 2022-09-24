ed15c24196678a5058680a21a41715a479f2b188
Parwiz Haqbien <parwiz.jan03@gmail.com>
2022-09-24 15:29:06 +1000

    Adding thr html and css

--- script.js
+++
@@ -1,7 +0,0 @@
-var searchValue = document.querySelector('#searchbox');
-searchValue.addEventListener('keypress' , setFunc);
-function setFunc(e) {
-  if(e.keypress ==13) {
-      console.log('Enter Pressed!!')
-  }
-}
