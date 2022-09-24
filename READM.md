4b00a6c9131da72dccaf2e2f969832c2fb0e59bb
Parwiz Haqbien <parwiz.jan03@gmail.com>
2022-09-24 14:51:44 +1000

    Ading the the js

--- READM.md
+++ READM.md
@@ -1,1 +1,2 @@
 # Weather-App
+For my Weather Dashboard I added a header along with text type and sumbit button so that users can search for city and their results will come up
--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var searchValue = document.querySelector('#')
+var searchValue = document.querySelector('#searchbox');
+searchValue.addEventListener('keypress' , setFunc);
+function setFunc(e) {
+  if(e.keypress ==13) {
+      console.log('Enter Pressed!!')
+  }
+}
